f1e422832692e231257d00d737a829114b8d01e4
SAPratz <scott.a.pratz@gmail.com>
2023-01-13 12:54:42 -0500

    added comments
    added comments

--- isDDR.py
+++ isDDR.py
@@ -1,3 +1,5 @@
+#this function will take a dog's URL and determine if the registration number of the dog is a DDR registration number. If there is no registration number listed, the program will set the value to 'RANDOM_PLACEHOLDER' and will not count that dog DDR until a DDR registration number is listed on a parent in PDB.
+
 def isDDR(child_url_string = ''):
 
     import requests
